0b716488e8723b5d2c53c7250f0f04ffb478c531
sammcode <58298401+sammcode@users.noreply.github.com>
2023-07-11 13:20:49 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "FieldDaySwiftUI",
+    name: "FieldDay",
     platforms: [
         .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "FieldDaySwiftUI",
-            targets: ["FieldDaySwiftUI"]),
+            name: "FieldDay",
+            targets: ["FieldDay"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,11 +22,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "FieldDaySwiftUI"
+            name: "FieldDay"
         ),
         .binaryTarget(
-            name: "FieldDaySwiftUI",
-            url: "https://github.com/fieldday-ai/FieldDaySwiftUI/releases/download/0.9.0/FieldDaySwiftUI-0.9.0.zip",
+            name: "FieldDay",
+            url: "https://github.com/fieldday-ai/FieldDaySwiftUI/releases/download/0.9.0/fieldday-ios-sdk-0.9.0.zip",
             checksum: "3a04ccfc13065e4ead12539f44b75a1945e32c83e8209bfce93af8e1e318a333"
         )
     ]
